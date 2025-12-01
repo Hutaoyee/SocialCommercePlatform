94df45321ec7f4a365e3020102b086fc725988b8
Hutaoyee <hutaoyee@foxmail.com>
2025-12-01 15:59:04 +0800

    fix depoly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' 
+    ? '/Vue3-Django---Social-Commerce-Platform/'
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
